fdd49a1c28236b64981d418e6f4e1ca5ae95096c
veve196 <veve196x@gmail.com>
2024-06-22 23:24:20 +0200

    boops

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -4,7 +4,34 @@ import Image from "next/image";
 import updateBoop from "./actions/updateBoop";
 
 export default function Avatar() {
-  const handleClick = async () => {
+  const handleClick = async (e: React.MouseEvent<HTMLImageElement>) => {
+    const text = document.createElement("span");
+
+    const emojis = [
+      "👉👈",
+      "🥺",
+      "❤",
+      "💕",
+      "💗",
+      "🧡",
+      "❣",
+      "💦",
+      "💙",
+      "💘",
+      "💓",
+      "im trying to reach you about your car's extended warranty",
+    ];
+
+    text.textContent = emojis[Math.floor(Math.random() * emojis.length)];
+    text.className = "fade-away";
+    text.style.left = `${e.pageX}px`;
+    text.style.top = `${e.pageY}px`;
+    document.body.appendChild(text);
+
+    setTimeout(() => {
+      document.body.removeChild(text);
+    }, 2000);
+
     const result = await updateBoop();
   };
 
@@ -15,8 +42,9 @@ export default function Avatar() {
       src="/veve.png"
       alt="veve"
       title="Goober"
-      className="rounded-full mx-auto"
-      onClick={handleClick}
+      draggable={false}
+      className="rounded-full mx-auto select-none"
+      onClick={(e) => handleClick(e)}
     />
   );
 }
