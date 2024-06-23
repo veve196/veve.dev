a61e5b14f7e7aba855d5395056798a388195162a
veve196 <veve196x@gmail.com>
2024-06-23 13:21:09 +0200

    brotkrümel

--- src/app/ref/page.tsx
+++ src/app/ref/page.tsx
@@ -2,14 +2,31 @@ import ColorButton from "@/components/colorButton";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export const runtime = "edge";
 export default async function Home() {
   return (
     <div className="w-[800px] mx-auto my-2 pt-4">
-      <Link href="/">
-        <Button>Go Back</Button>
-      </Link>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Ref</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
       <Image
         width={800}
         height={800}
