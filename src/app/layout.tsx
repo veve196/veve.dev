8a868396ef5ac88000497c9029c1b8e150092e73
veve196 <veve196x@gmail.com>
2024-06-23 18:26:36 +0200

    remove unn....ary scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} min-h-screen bg-background antialiased`}
-      >
+      <body className={`${inter.className} bg-background antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
