0be9f377da3b390e8feca7de8a0b207987b38a90
veve196 <veve196x@gmail.com>
2024-06-27 21:54:30 +0200

    boopcounter fix

--- src/components/boopCounter.tsx
+++ src/components/boopCounter.tsx
@@ -2,7 +2,7 @@
 
 import { client } from "@/app/appwrite";
 import { RealtimeResponseEvent } from "appwrite";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   count: number;
@@ -10,13 +10,13 @@ interface Props {
 
 export default function BoopCounter({ count }: Props) {
   const [boopCounter, setBoopCounter] = useState(count);
-
-  client.subscribe(
-    "databases.web.collections.counters.documents.veveBoops",
-    (response: RealtimeResponseEvent<any>) => {
-      setBoopCounter(response.payload.count);
-    }
-  );
-
+  useEffect(() => {
+    client.subscribe(
+      "databases.web.collections.counters.documents.veveBoops",
+      (response: RealtimeResponseEvent<any>) => {
+        setBoopCounter(response.payload.count);
+      }
+    );
+  }, []);
   return <p>Boops: {boopCounter}</p>;
 }
