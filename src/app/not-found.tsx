d6feb11f91ee395168b4741f8997e2346ca2b713
veve196 <veve196x@gmail.com>
2024-06-28 17:06:30 +0200

    brotkrümel

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,25 +2,49 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
 
 export default function Custom404() {
   return (
-    <div className="text-2xl flex flex-col items-center justify-center w-full h-full fixed overflow-hidden top-0 left-0 p-8">
-      <div className="flex flex-col items-center justify-center text-center gap-4">
-        <Image
-          src={"/404.webp"}
-          alt="Page not found"
-          title="Page not found"
-          width={400}
-          height={400}
-        />
-        <h1 className="text-8xl font-bold">404</h1>
-        <Separator className="w-full" />
-        <p>Oops! The page you&apos;re looking for was not found.</p>
-        <Button variant="link" className="text-2xl">
-          <Link href="/">Go back</Link>
-        </Button>
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href={"/"}>Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>???</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Separator className="my-4" />
+      <div className="text-2xl flex flex-col items-center justify-center w-full h-full fixed overflow-hidden top-0 left-0 p-8">
+        <div className="flex flex-col items-center justify-center text-center gap-4">
+          <Image
+            src={"/404.webp"}
+            alt="Page not found"
+            title="Page not found"
+            width={400}
+            height={400}
+          />
+          <h1 className="text-8xl font-bold">404</h1>
+          <Separator className="w-full" />
+          <p>Oops! The page you&apos;re looking for was not found.</p>
+          <Button variant="link" className="text-2xl">
+            <Link href="/">Go back</Link>
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
