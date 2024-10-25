02500b3f5bcd6119e65ed8b38b6647b18d3b422d
veve196 <veve196x@gmail.com>
2024-10-25 14:43:28 +0200

    build background component without sass

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,16 +1,57 @@
 "use client";
 
-import "../styles/background.scss";
+import { useEffect } from "react";
+import "../styles/background.css";
 
 export default function Background() {
+  useEffect(() => {
+    const stars = document.querySelectorAll(
+      "[id^=stars-]"
+    ) as NodeListOf<HTMLDivElement>;
+
+    stars.forEach((star: HTMLDivElement) => {
+      const [size, amount, speed] = star.id.split("-").slice(1).map(Number);
+
+      star.style.width = `${size}px`;
+      star.style.height = `${size}px`;
+
+      let boxShadow = "";
+
+      for (let i = 0; i < amount; i++) {
+        const x = Math.floor(Math.random() * 100);
+        const y = Math.floor(Math.random() * 100);
+        boxShadow += `${x}vw ${y}vh hsl(var(--secondary)), `;
+      }
+
+      boxShadow = boxShadow.slice(0, -2);
+
+      star.style.boxShadow = boxShadow.slice(0, -2);
+      star.style.animation = `animStar ${speed}s linear infinite`;
+
+      const afterRule = `
+      #${star.id}::after {
+        content: " ";
+        position: absolute;
+        top: 100vh;
+        width: ${size}px;
+        height: ${size}px;
+        background: transparent;
+        box-shadow: ${boxShadow};
+      }
+    `;
+
+      document.styleSheets[0].insertRule(
+        afterRule,
+        document.styleSheets[0].cssRules.length
+      );
+    });
+  }, []);
+
   return (
-    <div
-      id="background"
-      className="fixed w-full h-full z-[-1] top-0 left-0 overflow-hidden"
-    >
-      <div id="stars1"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
+    <div id="background">
+      <div id="stars-1-500-100"></div>
+      <div id="stars-2-200-150"></div>
+      <div id="stars-3-100-200"></div>
     </div>
   );
 }
