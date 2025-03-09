11a7b092f636ab89a732d6f49783699e11ee5765
veve196 <veve196x@gmail.com>
2025-03-09 21:08:08 +0100

    fix error  @typescript-eslint/no-namespace

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -1,101 +1,88 @@
 import { Models } from "node-appwrite";
-export namespace Statuses {
-  export interface StatusDocument extends Models.Document {
-    status: string;
-    from?: Date;
-    to?: Date;
-  }
 
-  export interface StatusType {
-    total: number;
-    documents: StatusDocument[];
-  }
+export interface StatusDocument extends Models.Document {
+  status: string;
+  from?: Date;
+  to?: Date;
 }
 
-export namespace Galleries {
-  export interface GalleryDocument extends Models.Document {
-    title: string;
-    description?: string;
-    isDefault?: boolean;
-    images: Images.ImageDocument[];
-  }
-
-  export interface GalleryType {
-    total: number;
-    documents: GalleryDocument[];
-  }
+export interface StatusType {
+  total: number;
+  documents: StatusDocument[];
 }
 
-export namespace Images {
-  export interface ImageDocument extends Models.Document {
-    fileId: string;
-    parentId?: string;
-    title: string;
-    description?: string;
-    artistUrl?: string;
-    sortOrder?: number;
-    isHidden?: boolean;
-    width?: number;
-    height?: number;
-    mimeType?: string;
-  }
-
-  export interface ImageType {
-    total: number;
-    documents: ImageDocument[];
-  }
+export interface GalleryDocument extends Models.Document {
+  title: string;
+  description?: string;
+  isDefault?: boolean;
+  images: ImageDocument[];
 }
 
-export namespace Milestones {
-  export interface MilestoneDocument extends Models.Document {
-    milestone: number;
-    message: string;
-  }
-
-  export interface MilestoneType {
-    total: number;
-    documents: MilestoneDocument[];
-  }
+export interface GalleryType {
+  total: number;
+  documents: GalleryDocument[];
 }
 
-export namespace Socials {
-  export interface SocialDocument extends Models.Document {
-    url: string;
-    title: string;
-    tooltip?: string;
-    sortOrder?: number;
-  }
-
-  export interface SocialType {
-    total: number;
-    documents: SocialDocument[];
-  }
+export interface ImageDocument extends Models.Document {
+  fileId: string;
+  parentId?: string;
+  title: string;
+  description?: string;
+  artistUrl?: string;
+  sortOrder?: number;
+  isHidden?: boolean;
+  width?: number;
+  height?: number;
+  mimeType?: string;
 }
 
-export namespace Highscores {
-  export interface HighscoreDocument extends Models.Document {
-    highscore: number;
-  }
-
-  export interface HighscoreType {
-    total: number;
-    documents: HighscoreDocument[];
-  }
+export interface ImageType {
+  total: number;
+  documents: ImageDocument[];
 }
 
-export namespace Discord {
-  export interface User {
-    id: string;
-    avatarUrl: string;
-    status: "online" | "idle" | "dnd" | "offline";
-  }
+export interface MilestoneDocument extends Models.Document {
+  milestone: number;
+  message: string;
+}
 
-  export interface SpotifyStatus {
-    title: string;
-    artist: string;
-    album: string;
-    coverUrl: string;
-    startDate: Date;
-    endDate: Date;
-  }
+export interface MilestoneType {
+  total: number;
+  documents: MilestoneDocument[];
+}
+
+export interface SocialDocument extends Models.Document {
+  url: string;
+  title: string;
+  tooltip?: string;
+  sortOrder?: number;
+}
+
+export interface SocialType {
+  total: number;
+  documents: SocialDocument[];
+}
+
+export interface HighscoreDocument extends Models.Document {
+  highscore: number;
+}
+
+export interface HighscoreType {
+  total: number;
+  documents: HighscoreDocument[];
+}
+
+export interface DiscordUser {
+  id: string;
+  avatarUrl: string;
+  status: "online" | "idle" | "dnd" | "offline";
+}
+
+export interface SpotifyStatus {
+  title: string;
+  artist: string;
+  album: string;
+  coverUrl: string;
+  startDate: Date;
+  endDate: Date;
 }
