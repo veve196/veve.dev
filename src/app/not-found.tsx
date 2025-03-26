cb7ed4ffdb2db938f619935ecbf25e17ed3b8ae7
veve196 <veve196x@gmail.com>
2025-03-26 10:02:01 +0100

    missing picture on 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -37,11 +37,11 @@ export default function NotFound() {
       <div className="text-2xl flex flex-col items-center justify-center w-full h-full fixed overflow-hidden top-0 left-0 p-8">
         <div className="flex flex-col items-center justify-center text-center gap-4">
           <Image
-            src={"/404.webp"}
+            src={"/not-found/not-found.webp"}
             alt="Page not found"
             title="Page not found"
-            width={400}
-            height={400}
+            width={200}
+            height={200}
           />
           <h1 className="text-8xl font-bold">404</h1>
           <Separator className="w-full" />
