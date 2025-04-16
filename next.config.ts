b7d4180e4096ab57fca5f1d2aafb38b352a2622a
veve196 <veve196x@gmail.com>
2025-04-16 14:02:45 +0200

    prep

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.scdn.co",
       },
+      {
+        protocol: "https",
+        hostname: "api.veve.dev",
+      },
     ],
   },
 };
