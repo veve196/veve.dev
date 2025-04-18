f183c381bd116d9f361c179a225401451b4dcac6
veve196 <veve196x@gmail.com>
2025-04-18 22:47:33 +0200

    lint error

--- src/app/(subpages)/gallery/(with-template)/template.tsx
+++ src/app/(subpages)/gallery/(with-template)/template.tsx
@@ -1,5 +1,4 @@
 import GalleryToggleGroup from "@/components/gallery-toggle-group";
-import { GalleryType } from "@/utils/models";
 import { getGalleries } from "@/server-api/gallery";
 
 interface GalleryTemplateProps {
