962277ea72a6e7d5a6adac65385fe871ee4badc0
veve196 <veve196x@gmail.com>
2025-04-18 21:21:40 +0200

    sorry

--- src/components/ballpit.tsx
+++ src/components/ballpit.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+
 "use client";
 
 import React, { useRef, useEffect } from "react";
