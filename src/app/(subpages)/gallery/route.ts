612732b19d20f13b6292c595bec30944a76ac85f
veve196 <veve196x@gmail.com>
2025-04-18 22:50:22 +0200

    bugfix

--- src/app/(subpages)/gallery/route.ts
+++ src/app/(subpages)/gallery/route.ts
@@ -3,8 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const defaultGallery = await getDefaultGallery();
+  let url;
 
-  return NextResponse.redirect(
-    new URL(`/gallery/${defaultGallery.$id}`, request.url)
-  );
+  if (defaultGallery == null) {
+    url = new URL("/", request.url);
+  } else {
+    url = new URL(`/gallery/${defaultGallery.$id}`, request.url);
+  }
+
+  return NextResponse.redirect(url);
 }
