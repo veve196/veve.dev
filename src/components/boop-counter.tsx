15d69f99c630de4ea83e957bcba0a33babee74eb
veve196 <veve196x@gmail.com>
2025-04-18 19:52:05 +0200

    styles

--- src/components/boop-counter.tsx
+++ src/components/boop-counter.tsx
@@ -5,8 +5,10 @@ import getBoops from "@/client-api/getBoops";
 import { CounterDocument } from "@/utils/models";
 import { RealtimeResponseEvent } from "appwrite";
 import { useEffect, useState } from "react";
+import CountUp from "@/components/count-up";
 
 export default function BoopCounter() {
+  const [prevBoops, setPrevBoops] = useState<number>(0);
   const [boops, setBoops] = useState<number | null>(null);
 
   useEffect(() => {
@@ -16,8 +18,21 @@ export default function BoopCounter() {
       "databases.web.collections.counters.documents.veveBoops",
       (response: RealtimeResponseEvent<CounterDocument>) => {
         setBoops(response.payload.count);
+        setPrevBoops(boops || 0);
       }
     );
   }, []);
-  return <p>Boops: {boops || "..."}</p>;
+  return (
+    <>
+      <p className="inline">Boops: </p>
+      <CountUp
+        from={prevBoops}
+        to={boops || 0}
+        separator=","
+        direction="up"
+        duration={1}
+        className="count-up-text"
+      />
+    </>
+  );
 }
