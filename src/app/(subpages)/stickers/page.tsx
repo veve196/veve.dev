dba433b6588b4858cd5d26fb5550533d6ebb638b
veve196 <veve196x@gmail.com>
2025-08-07 10:25:41 +0200

    minor change on sticker page

--- src/app/(subpages)/stickers/page.tsx
+++ src/app/(subpages)/stickers/page.tsx
@@ -12,19 +12,32 @@ export const metadata: Metadata = {
 };
 
 export default function Stickers() {
-  const stickerPack = "FurdisAndVeve";
+  const stickerPack = process.env.TELEGRAM_STICKER_PACK;
 
+  if (!stickerPack)
+    return (
+      <>
+        <h1 className="text-center text-4xl mb-8">Stickers not found!</h1>
+        <p className="text-center">
+          Tell veve to set the{" "}
+          <code className="bg-black/10 p-2 font-[inherit]">
+            TELEGRAM_STICKER_PACK
+          </code>{" "}
+          environment variable... :3
+        </p>
+      </>
+    );
   return (
     <>
       <div className="text-center">
         <h1 className="text-4xl mb-2 text-rainbow">My Stickers!</h1>
         <p className="mb-8">
-          Thanks for checking them out, you can get them{" "}
+          Thanks for checking them out, you can get them&nbsp;
           <Link
             href={`https://t.me/addstickers/${stickerPack}`}
             target="_blank"
             title="My Stickers"
-            className="bg-gradient-to-r from-violet-500 via-blue-500 to-pink-500 bg-clip-text text-transparent animate-pulse"
+            className="bg-gradient-to-r from-violet-500 via-blue-500 to-pink-500 bg-clip-text text-transparent font-bold animate-pulse"
           >
             here
           </Link>
